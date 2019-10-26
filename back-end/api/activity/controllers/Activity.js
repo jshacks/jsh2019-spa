5928c357f3b457e27239ffaadae64f57a3150768
axbg <bisagalexstefan@gmail.com>
2019-10-26 18:16:00 +0300

    Added getActivitiesPerSubject

--- back-end/api/activity/controllers/Activity.js
+++ back-end/api/activity/controllers/Activity.js
@@ -1,7 +1,17 @@
 'use strict';
-
+const groupArray = require('group-array');
+const _ = require('lodash');
 /**
  * Read the documentation () to implement custom controller functions
  */
 
-module.exports = {};
+module.exports = {
+    activitiesPerSubject: async (ctx) => {
+        const activities = await Activity.find();
+        const grouped = _.groupBy(activities, function(ob) {
+            return ob.subject;
+        });
+
+        ctx.body = {activities: grouped}
+    }
+};
