7e6da49a5181dbeb3a77320efb6f4a71a83099a2
Balaur <zadelaz225@gmail.com>
2019-10-26 12:19:33 +0300

    added custom fonts & login page wip

--- front-end/src/pages/LoginPage.vue
+++ front-end/src/pages/LoginPage.vue
@@ -1,17 +1,42 @@
 <template>
   <q-page class="flex flex-center">
-    <q-btn
-      align="around"
-      class="btn-fixed-width"
-      color="brown-5"
-      label="Login"
-      icon="exit_to_app"
-      @click="$router.push('/')"
-    />
+    <div class="container">
+      <div class="description-area">
+        <h3 class="text-rubik">Schedule Planner App</h3>
+        <p class="text-subtitle1">Fa din studentia ta o perioada minunata!</p>
+      </div>
+      <div class="login-area"></div>
+    </div>
   </q-page>
 </template>
 
-<style></style>
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  background-color: darkgoldenrod;
+  /* background-image: url("../assets/img/background.svg");
+  background-repeat: no-repeat;
+  background-position: fixed;
+  background-size: cover; */
+}
+.description-area,
+.login-area {
+  flex-basis: 0;
+  flex-grow: 1;
+}
+.description-area {
+  background: darkgreen;
+}
+.login-area {
+  display: flex;
+  background: darkred;
+  justify-content: center;
+  align-items: center;
+}
+</style>
 
 <script>
 export default {
