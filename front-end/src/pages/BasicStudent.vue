78c3460b90eeef8b30d722cd423694c855bbccf0
Balaur <zadelaz225@gmail.com>
2019-10-26 16:07:58 +0300

    besic Student page wip

--- front-end/src/pages/BasicStudent.vue
+++ front-end/src/pages/BasicStudent.vue
@@ -8,16 +8,56 @@
       align="justify"
       narrow-indicator
     >
-      <q-tab name="mails" label="Mails" />
-      <q-tab name="alarms" label="Alarms" />
+      <q-tab name="groupSchedule" label="Group" />
+      <q-tab name="personalSchedule" label="Personal" />
     </q-tabs>
 
     <q-tab-panels v-model="tab" animated>
-      <q-tab-panel name="mails">
-        <div class="text-h6">Mails</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <q-tab-panel name="groupSchedule">
+        <div class="schedule-slot">
+          <q-card>
+            <q-tabs
+              v-model="tabInside"
+              dense
+              class="text-grey"
+              active-color="primary"
+              indicator-color="primary"
+              align="justify"
+              narrow-indicator
+            >
+              <q-tab name="monday" label="L" />
+              <q-tab name="tuesday" label="M" />
+              <q-tab name="wednesday" label="M" />
+              <q-tab name="thursday" label="J" />
+              <q-tab name="friday" label="V" />
+            </q-tabs>
+
+            <q-separator />
+
+            <q-tab-panels v-model="tabInside" animated>
+              <q-tab-panel name="monday">
+                <div class="text-h6">monday</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="tuesday">
+                <div class="text-h6">tuesday</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="wednesday">
+                <div class="text-h6">wednesday</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+              <q-tab-panel name="thursday">
+                <div class="text-h6">thursday</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+              <q-tab-panel name="friday">
+                <div class="text-h6">friday</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+            </q-tab-panels>
+          </q-card>
+        </div>
       </q-tab-panel>
 
-      <q-tab-panel name="alarms">
+      <q-tab-panel name="personalSchedule">
         <div class="text-h6">Alarms</div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </q-tab-panel>
     </q-tab-panels>
@@ -29,6 +69,14 @@
   width: 100%;
   margin-top: 10px;
 }
+.q-card {
+  width: 100%;
+}
+.schedule-slot {
+  width: 90%;
+  border-radius: 5%;
+  background-color: #f5f5f5;
+}
 </style>
 
 <script>
@@ -39,7 +87,8 @@ export default {
   },
   data() {
     return {
-      tab: "mails"
+      tab: "groupSchedule",
+      tabInside: "mails"
     };
   },
   computed: {
